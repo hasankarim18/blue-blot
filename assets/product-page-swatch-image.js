ef017cf421698ec203f30c74acab9da3bb6387ab
hasankarim18 <hallysust@gmail.com>
2025-03-15 20:55:04 +0600

    swatch add to cart

--- assets/product-page-swatch-image.js
+++ assets/product-page-swatch-image.js
@@ -38,7 +38,11 @@ class ProductPageSwatchImage extends HTMLElement {
     this.variantSelector.addEventListener("change", () => {
       this.getSelectedOptions();
       this.getSelectedVarint();
-      console.log(this.options);
+      if (this.currentVariant) {
+        this.updateUrlAndVariantId();
+        this.updateVariantTitle();
+        this.updateUi();
+      }
     });
   }
 
@@ -50,13 +54,84 @@ class ProductPageSwatchImage extends HTMLElement {
     this.options = Array.from(inputs, (radio) => radio.value);
   }
   //stet-2 get variant json
-  getVariantJson() {}
+  getVariantJson() {
+    this.variandData =
+      this.variandData ||
+      JSON.parse(
+        this.variantSelector.querySelector(`[type="application/json"]`)
+          .textContent
+      );
+    return this.variandData;
+  }
   //stet-3 get selected variant
-  getSelectedVarint() {}
+  getSelectedVarint() {
+    this.currentVariant = this.getVariantJson().find((variant) => {
+      //  console.log(variant);
+      const findings = variant.options.every((option, index) => {
+        return this.options[index] === option;
+      });
+      if (findings) return findings;
+    });
+  }
   // step-4 updateUrl
-  updateUrl() {}
+  updateUrlAndVariantId() {
+    if (!this.currentVariant) return;
+    const variantIdInput = this.querySelector(`[name="id"]`);
+    variantIdInput.value = this.currentVariant.id;
+    window.history.replaceState(
+      {},
+      "",
+      `${this.dataset.url}?variant=${this.currentVariant.id}`
+    );
+  }
+
+  // update Variant title
+  updateVariantTitle() {
+    const variantTitle = this.querySelector(`#variant_title_${this.sectionId}`);
+    variantTitle.innerHTML = this.currentVariant.title;
+  }
+
   // step-5 updatUi
-  updateUi() {}
+  updateUi() {
+    if (!this.currentVariant) return;
+    const variantId = this.currentVariant.id;
+    const productUrl = this.dataset.url;
+    const sectionId = this.sectionId;
+    const spinner = this.querySelector(`#spinner_${this.sectionId}`);
+
+    //
+    spinner.classList.remove("hidden");
+    spinner.classList.add("flex");
+    // fetch
+    fetch(`${productUrl}?variant=${variantId}&section_id${this.sectionId}`)
+      .then((response) => response.text())
+      .then((responseText) => {
+        const parser = new DOMParser();
+        const html = parser.parseFromString(responseText, "text/html");
+        // update price
+        const oldPrice = this.querySelector(`#price_${this.sectionId}`);
+        const newPrice = html.querySelector(`#price_${this.sectionId}`);
+        if (oldPrice && newPrice) oldPrice.innerHTML = newPrice.innerHTML;
+
+        // update cart action
+        const oldCartAction = this.querySelector(
+          `#cart_action_${this.sectionId}`
+        );
+        const newCartAction = html.querySelector(
+          `#cart_action_${this.sectionId}`
+        );
+
+        if (oldCartAction && newCartAction)
+          oldCartAction.innerHTML = newCartAction.innerHTML;
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        spinner.classList.add("hidden");
+        spinner.classList.remove("flex");
+      });
+  }
 
   //show swatch image or color
   showColorSwatchName() {
